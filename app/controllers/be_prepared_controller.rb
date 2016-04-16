d2df167d59e4ab594fc7e696d4a2d333f33d40c8
Giorgos Ioannidis <g.ioannidi5@yahoo.co.uk>
2016-04-16 23:02:37 +0300

    retrieve data from api

--- app/controllers/be_prepared_controller.rb
+++ app/controllers/be_prepared_controller.rb
@@ -12,28 +12,34 @@ class BePreparedController < ApplicationController
   end
 
   def estimation_flight
-    require 'rubygems'
-    require 'geokit'
-    require 'httparty'
 
+    if(params[:search].present?)
+      require 'rubygems'
+      require 'geokit'
+      require 'httparty'
+      @title = "Flight Estimation"
 
+      if(params[:search]=="")
+        flash[:alert] = "You have to enter a location"
+        redirect_to root_path
+      else
+        @location = Geokit::Geocoders::GoogleGeocoder.geocode (params[:search])
+        url ="http://api.wunderground.com/api/5640750144c0346d/conditions/q/"+@location.lat.to_s+","+@location.lng.to_s+".json"
+        response = HTTParty.get(url)
+        response.parsed_response
 
+        if(response["response"]["error"].present?)
+          flash[:alert] = "Invalid location"
+          redirect_to root_path
+        else
 
-
-
-    @title = "Flight Estimation"
-
-
-    @location = Geokit::Geocoders::GoogleGeocoder.geocode (params[:search])
-
-    url ="http://api.wunderground.com/api/5640750144c0346d/conditions/q/"+@location.lat.to_s+","+@location.lng.to_s+".json"
-    response = HTTParty.get(url)
-    response.parsed_response
-    @results = response
-
-    
-
-
+          @results = response
+        end
+      end
+    else
+        flash[:alert] = "Unauthorised Access"
+        redirect_to root_path
+    end
 
   end
 end
